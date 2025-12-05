674819791cba453a94850e4805db593f086e69d6
rayyankadiwal-maker <rayyankadiwal@eng.rizvi.edu.in>
2025-12-05 14:18:04 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,16 @@
+/**name: Rayyan A Kadiwal
+Div:F 
+Branch:Mechanical
+Roll No: 20
+UIN NO:251M023**/
+#include <stdio.h>
+
+int main() {
+    printf("Name\t: Rayyan A Kadiwal\n");
+    printf("Age\t: 18\n");
+    printf("Class\t: FE Mechanical \n");
+    printf("Division: F\n");
+    printf("UIN\t: 251M033\n");
+
+    return 0;
+}
